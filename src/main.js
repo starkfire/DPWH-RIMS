3d4bb4a628205c9dbbf90acf59797e2f648ce95b
starkfire <kingbeyondthenorth@gmail.com>
2020-02-05 03:31:57 +0800

    setup main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+Vue.use(ElementUI)
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  el: '#app',
+  render: h => h(App)
+})
