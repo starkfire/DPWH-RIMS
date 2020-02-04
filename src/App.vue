4620ecd20e83bbb070c92a3b4263c1cf099abd1f
starkfire <kingbeyondthenorth@gmail.com>
2020-02-05 06:14:40 +0800

    add base template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header style="padding: 0px;">
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#111"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            style="border-bottom: 0px; padding-left: 145px;"
+        >
+          <el-menu-item index="1" @click="navigate('/')">Home</el-menu-item>
+          <el-menu-item index="2" @click="navigate('/dashboard')">Dashboard</el-menu-item>
+          <el-menu-item index="3" @click="navigate('/settings')">Settings</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main style="padding: 0px;">
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import router from './router'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data(){
+        return {
+            activeIndex: '1'
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath){
+            console.log(key, keyPath)
+        },
+        navigate(to){
+            router.push({ path: to })
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'CeraPro Light';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body{
+  margin: 0px;
+  background-color: #111;
 }
 </style>
