454f98f00fe2488f5a652bbd1b057029fd0e0293
starkfire <kingbeyondthenorth@gmail.com>
2020-03-23 19:12:33 +0800

    update Manager

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -2,11 +2,11 @@
     <div class="asset-manager">
         <a-row :gutter="12">
             <a-col :span="14">
-                <p id="header">Inventory</p>
+                <p id="header">Manager</p>
             </a-col>
             <a-col :span="8">
                 <br>
-                <a-input-search placeholder="Search Data" @search="onSearch" style="float:right;" />
+                <a-input-search placeholder="Search Data" style="float:right;" />
             </a-col>
             <a-col :span="2">
                 <br>
@@ -22,9 +22,8 @@
                             <a-upload
                                 name="file"
                                 accept=".csv"
-                                action="http://localhost:3000/upload/"
+                                action="http://127.0.0.1:3000/api/asset/upload"
                                 :multiple="false"
-                                :headers="headers"
                                 @change="handleChange"
                             >
                                 <a-button><a-icon type="cloud-upload" />Upload</a-button>
@@ -44,37 +43,78 @@
                 </a-modal>
             </a-col>
         </a-row>
+        <a-row>
+            <a-table
+                :columns="columns"
+                :rowKey="tableData => tableData._id"
+                :dataSource="tableData"
+                :loading="loading"
+            >
+                <span slot="date" slot-scope="item">
+                    {{ item.split('T')[0] }}
+                </span>
+                <span slot="time" slot-scope="item">
+                    {{ item.split('T')[1].split('.')[0] }}
+                </span>
+            </a-table>
+        </a-row>
     </div>
 </template>
 <script>
+import * as axios from 'axios'
+
+const columns = [
+    {
+        title: 'Date',
+        key: 'date',
+        dataIndex: 'date',
+        scopedSlots: { customRender: 'date' }
+    },
+    {
+        title: 'Time',
+        key: 'time',
+        dataIndex: 'date',
+        scopedSlots: { customRender: 'time' }
+    },
+    {
+        title: 'Type',
+        key: 'type',
+        dataIndex: 'type'
+    },
+    {
+        title: 'Value',
+        key: 'type',
+        dataIndex: 'type'
+    }
+]
+
+
 export default {
-    name: 'Potholes',
-    data(){
+    name: 'Manager',
+    data() {
         return {
-            headers: {
-                authorization: 'authorization'
-            },
-            modalVisible: false
+            loading: false,
+            modalVisible: false,
+            tableData: [],
+            columns
         }
     },
     methods: {
         handleRemove(file, fileList) {
             console.log(file, fileList)
         },
-        onSearch(value){
-            console.log(value)
-        },
-        handleChange(info){
+        handleChange(info) {
             if(info.file.status !== 'uploading'){
                 console.log(info.file, info.fileList)
             }
             if(info.file.status === 'done'){
                 this.$message.success('Upload Success')
-            }else if(info.file.status === 'error'){
+            } else if(info.file.status === 'error') {
                 this.$message.error(`${info.file.response.message}`)
             }
         }
     }
+    
 }
 </script>
 <style scoped>
